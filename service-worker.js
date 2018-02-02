828e0c1de4a4b005927001f213b4f075e71bcfb8
marabesi <matheus.marabesi@gmail.com>
2018-02-02 21:24:04 -0200

    removed cache response till fix the sw flow

--- service-worker.js
+++ service-worker.js
@@ -16,9 +16,9 @@ self.addEventListener('fetch', function (event) {
     event.respondWith(
         caches.match(event.request)
             .then(function (response) {
-                if (response) {
-                    return response;
-                }
+                // if (response) {
+                //     return response;
+                // }
 
                 var fetchRequest = event.request.clone();
 
