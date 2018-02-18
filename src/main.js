9022743012b28f7362bc35835cc9b99b789ec696
marabesi <matheus.marabesi@gmail.com>
2018-02-18 18:32:19 -0300

    fixed bug that doesn't allowed to open the menu inside post page, moved the disqus javascript to the main js file and updated the polymer component version

--- src/main.js
+++ src/main.js
@@ -23,6 +23,15 @@
         document.querySelector('paper-icon-item').addEventListener('click', function() {
             drawer.toggle();
         });
+
+        if (document.getElementById('disqus_thread')) {
+            var d = document, s = d.createElement('script');
+
+            s.src = '//marabesi.disqus.com/embed.js';
+
+            s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+        }
     };
 
     onload();
