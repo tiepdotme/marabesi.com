7b2e136d3eed5ee2467fc539442c86b80f59a93c
marabesi <matheus.marabesi@gmail.com>
2019-04-03 20:57:41 +0200

    addedjs function to open/close the menu

--- src/main.js
+++ src/main.js
@@ -37,6 +37,19 @@
             s.setAttribute('data-timestamp', +new Date());
                 (d.head || d.body).appendChild(s);
         }
+
+        const menuList = document.querySelector('#menu-list')
+
+        document.querySelector('#menu').addEventListener('click', function() {
+            if (menuList.classList.contains('flex')) {
+                menuList.classList.remove('flex')
+                menuList.classList.add('hidden')
+            } else {
+                menuList.classList.remove('hidden')
+                menuList.classList.add('flex')
+                menuList.classList.add('open')
+            }
+        });
     };
 
     onload();
