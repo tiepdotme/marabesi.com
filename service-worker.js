5d20f178841feddb15cc72c87d6e284b69c29d1b
marabesi <matheus.marabesi@gmail.com>
2019-05-15 09:21:01 +0200

    service worker version updated

--- service-worker.js
+++ service-worker.js
@@ -1,63 +1,63 @@
-var CACHE_NAME = 'marabesi.com_v3';
+var CACHE_NAME = 'marabesi.com_v5';
 var assetsToCache = [
-    '/',
+  '/',
 ];
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(assetsToCache);
-            })
-    );
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        return cache.addAll(assetsToCache);
+      })
+  );
 });
 
 self.addEventListener('activate', function (event) {
-    event.waitUntil(
-        caches.keys().then(function (cacheNames) {
-            return Promise.all(
-                cacheNames.filter(function (cacheName) {
-                    return cacheName.startsWith('marabesi.com_') &&
-                        cacheName != CACHE_NAME;
-                }).map(function (cacheName) {
-                    return caches.delete(cacheName);
-                })
-            );
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function (cacheName) {
+          return cacheName.startsWith('marabesi.com_') &&
+            cacheName != CACHE_NAME;
+        }).map(function (cacheName) {
+          return caches.delete(cacheName);
         })
-    );
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function (response) {
-                 if (response) {
-                    return response;
-                }
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
 
-                var fetchRequest = event.request.clone();
+        var fetchRequest = event.request.clone();
 
-                return fetch(fetchRequest).then(
-                    function (response) {
-                        if (!response) {
-                            return response;
-                        }
+        return fetch(fetchRequest).then(
+          function (response) {
+            if (!response) {
+              return response;
+            }
 
-                        var responseToCache = response.clone();
+            var responseToCache = response.clone();
 
-                        caches.open(CACHE_NAME)
-                            .then(function (cache) {
-                                cache.put(event.request, responseToCache);
-                            });
+            caches.open(CACHE_NAME)
+              .then(function (cache) {
+                cache.put(event.request, responseToCache);
+              });
 
-                        return response;
-                    });
-            })
-    );
+            return response;
+          });
+      })
+  );
 });
 
-self.addEventListener('message', function(event) {
-    if (event.data.action === 'skip') {
-        self.skipWaiting();
-    }
+self.addEventListener('message', function (event) {
+  if (event.data.action === 'skip') {
+    self.skipWaiting();
+  }
 });
