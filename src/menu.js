cf668634268f9b5ae78fcf309ced068ff123cb17
marabesi <matheus.marabesi@gmail.com>
2019-07-07 21:32:18 +0200

    fix open/close menu

--- src/menu.js
+++ src/menu.js
@@ -1,12 +1,22 @@
+const topMenu = document.querySelector('.top-menu')
 const menuList = document.querySelector('#menu-list')
 
 document.querySelector('#menu').addEventListener('click', function () {
     if (menuList.classList.contains('flex')) {
-    menuList.classList.remove('flex')
-    menuList.classList.add('hidden')
+        menuList.classList.remove('flex')
+        menuList.classList.add('hidden')
+
+        topMenu.classList.remove('flex')
+        topMenu.classList.add('hidden')
+
+        menuList.classList.remove('open')
     } else {
-    menuList.classList.remove('hidden')
-    menuList.classList.add('flex')
-    menuList.classList.add('open')
+        menuList.classList.remove('hidden')
+        menuList.classList.add('flex')
+
+        topMenu.classList.remove('hidden')
+        topMenu.classList.add('flex')
+
+        menuList.classList.add('open')
     }
 });
