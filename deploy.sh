573590431418df1cd2abea958f88253a80b093aa
marabesi <matheus.marabesi@gmail.com>
2019-07-07 11:41:09 +0200

    added -f flag to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ remote_branch="gh-pages" && \
 git init && \
 git config user.name "marabesi" && \
 git config user.email "matheus.marabesi@gmail.com" && \
-git add . && \
-git commit -m'build' && \
+git add -f .  && \
+git commit -m 'build' && \
 git push --force --quiet $remote_repo master:$remote_branch > /dev/null 2>&1 && \
 rm -fr .git && \
 cd ../
