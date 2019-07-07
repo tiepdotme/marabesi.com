109f4ef9129a6aeeccd73e861b53efea5f01d727
marabesi <matheus.marabesi@gmail.com>
2019-07-07 11:53:52 +0200

    moved webpack asserts result to _site/assets

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   ],
   output: {
     filename: '[name]-bundle.js',
-    path: path.resolve(__dirname, 'assets')
+    path: path.resolve(__dirname, '_site/assets')
   },
   module: {
     rules: [{
