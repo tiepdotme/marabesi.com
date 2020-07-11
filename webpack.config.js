734e406d85112640033e94c90f37a23184075e4d
marabesi <matheus.marabesi@gmail.com>
2020-07-11 23:20:01 +0200

    ci: enabled purgecss in production only

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,32 @@ const glob = require('glob')
 const PurgecssPlugin = require('purgecss-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const purgePath = `${path.resolve(__dirname)}/**/{*.html, *.css}`
+const NODE_ENV = process.env.NODE_ENV
+
 class TailwindExtractor {
   static extract(content) {
-    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || []
   }
+}
+
+const plugins = [
+  new MiniCssExtractPlugin({
+    filename: "[name]-bundle.css",
+  }),
+];
+
+if (NODE_ENV !== 'development') {
+  plugins.push(
+    new PurgecssPlugin({
+      paths: glob.sync(purgePath, { nodir: true, ignore: '{node_modules,vendor}/**' }),
+      extractors: [
+        {
+          extractor: TailwindExtractor,
+          extensions: ['html', 'js'],
+        },
+      ],
+    }),
+  )
 }
 
 module.exports = {
@@ -45,19 +67,5 @@ module.exports = {
   resolve: {
     extensions: ['.json', '.js'],
   },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name]-bundle.css",
-    }),
-    new PurgecssPlugin({
-      paths: glob.sync(purgePath, { nodir: true, ignore: '{node_modules,vendor}/**' }),
-      extractors: [
-        {
-          extractor: TailwindExtractor,
-
-          extensions: ['html', 'js'],
-        },
-      ],
-    }),
-  ]
-};
+  plugins,
+}
